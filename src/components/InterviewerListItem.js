3ca1b8ba0d3c23be53705a07c79a839999b388c2
Linh Hoang <ellehoang@hotmail.com>
2021-12-11 14:17:01 -0500

    replace hard-coded values with props, add onClick handler to <li>, conditionally add classes, and conditionally render interviewer's name in InterviewerListItem component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,16 +1,24 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/InterviewerListItem.scss";
 
+// InterviewerListItem receive props: ( id: num - id of interviewer, name:string - name of interviewer, avatar: url - url to img of interviewer )
+// InterviewerListItme need prop to know if selected ( selected:boolean - determines if interviewer is selected or not & display name & apply style of selected )
+// InterviewerListItem receive function = setInterviewer ( setInterviewer:function - runs when InterviewerListItem is clicked. Receives interviewer's id as argument. sets selected interviewer )
 export default function InterviererListItem(props) {
+  let interviewerClass = classNames("interviewers__item", {
+    " interviewers__item--selected": props.selected
+  });
+
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}> {/* rep. interviewer item */}
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
-      />
-      Sylvia Palmer
+        src={props.avatar}
+        alt={props.name}
+      /> {/* interviewer img */}
+      {props.selected && props.name} {/* conditionally render interviewer's name using short circuit evaluation */}
     </li>
   );
 }
