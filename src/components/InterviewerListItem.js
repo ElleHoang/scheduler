44a5e097e6673f788e1cf4bf833364780ba5a637
Linh Hoang <ellehoang@hotmail.com>
2021-12-11 16:57:35 -0500

    correct InterviewerListItem component typo

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -6,7 +6,7 @@ import "components/InterviewerListItem.scss";
 // InterviewerListItem receive props: ( id: num - id of interviewer, name:string - name of interviewer, avatar: url - url to img of interviewer )
 // InterviewerListItme need prop to know if selected ( selected:boolean - determines if interviewer is selected or not & display name & apply style of selected )
 // InterviewerListItem receive function = setInterviewer ( setInterviewer:function - runs when InterviewerListItem is clicked. Receives interviewer's id as argument. sets selected interviewer )
-export default function InterviererListItem(props) {
+export default function InterviewerListItem(props) {
   let interviewerClass = classNames("interviewers__item", {
     " interviewers__item--selected": props.selected
   });
