c46e39a9143bc00a1678bd23c658c27f3d8818b3
Linh Hoang <ellehoang@hotmail.com>
2021-12-12 02:14:44 -0500

    refactor InterviewerList and InterviewerListItem to no longer pass id prop to InterviewerListItem

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,14 +4,6 @@ import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
-  
-  const interviewers = [
-    { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-    { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-    { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-    { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-    { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-  ];
 
   // InterviewerList receives three props:
   // 1. interviewers:array - array of interviewer objects
@@ -20,13 +12,18 @@ export default function InterviewerList(props) {
   
   // build component that returns array of InterviewerListItem components
   // each item in list will need unique key prop
-  const parsedInterviewers = interviewers.map(interviewer =>
-  <InterviewerListItem
-    key={interviewer.id}
-    name={interviewer.name}
-    avatar={interviewer.avatar}
-    selected={interviewer.id === props.interviewer} setInterviewer={events => props.setInterviewer(interviewer.id)} />
+  const parsedInterviewers = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
+      />
     );
+  });
+  
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -7,12 +7,12 @@ import "components/InterviewerListItem.scss";
 // InterviewerListItme need prop to know if selected ( selected:boolean - determines if interviewer is selected or not & display name & apply style of selected )
 // InterviewerListItem receive function = setInterviewer ( setInterviewer:function - runs when InterviewerListItem is clicked. Receives interviewer's id as argument. sets selected interviewer )
 export default function InterviewerListItem(props) {
-  let interviewerClass = classNames("interviewers__item", {
+  const interviewerClass = classNames("interviewers__item", {
     " interviewers__item--selected": props.selected
   });
 
   return (
-    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}> {/* rep. interviewer item */}
+    <li className={interviewerClass} onClick={props.setInterviewer}> {/* rep. interviewer item */}
       <img
         className="interviewers__item-image"
         src={props.avatar}
