2c2ef00a72d7996aa145a68cb31cab065f9bcf0f
Linh Hoang <ellehoang@hotmail.com>
2021-12-13 01:11:53 -0500

    use controlled component to capture student name and update it in input tag, and use controlled list to assign value and onChange props to capture interviewer and update it in InterviewerList

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -17,14 +17,14 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-              your code goes here
-            */
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList 
-          /* your code goes here */
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
