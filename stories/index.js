01c5e06b6c2247b1f5ecac76cb5ffbb2c90f871f
Linh Hoang <ellehoang@hotmail.com>
2021-12-12 22:41:42 -0500

    build Show component, chain and write Show story under Appointment story

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 
 // Button stories describing what each part of story does (actions of buttons)
@@ -157,4 +158,12 @@ storiesOf("Appointment", module)
 .add("Appointment", () => <Appointment text="No Appointment" />)
 .add("Appointment with Time", () => <Appointment time ="Appointment at 12pm" />)
 .add("Header", () => <Header time="Appointment at 12pm" />)
-.add("Empty", () => <Empty onAdd={action("onAdd")} />);
+.add("Empty", () => <Empty onAdd={action("onAdd")} />)
+.add("Show", () =>
+  <Show
+    student="Lydia Miller-Jones"
+    interviewer={interviewer}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+  />
+);
