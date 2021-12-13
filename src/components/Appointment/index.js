e9ea2f0a9fe4ebb24e1fa63f8061e3fcc3991eb8
Linh Hoang <ellehoang@hotmail.com>
2021-12-13 03:01:51 -0500

    import Fragment into storybook file, add Appointment Empty story, and implement codes to render Header, and Show or Empty components in Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,17 @@
 import React from "react";
+
 import "components/Appointment/styles.scss";
+
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
 
   return (
     <article className="appointment">
-      {props.text}
-      {props.time}
+      <Header time={props.time} />
+      {props.interview ? <Show /> : <Empty />}
     </article>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -212,7 +212,7 @@ storiesOf("Appointment", module)
     interviewer={1}
     interviewers={interviewers}
     onSave={action("onSave")}
-    onCanel={action("onCancel")}
+    onCancel={action("onCancel")}
   />
 ))
 .add("Create", () => (
@@ -221,4 +221,10 @@ storiesOf("Appointment", module)
     onSave={action("onSave")}
     onCancel={action("onCancel")}
   />
+))
+.add("Appointment Empty", () => (
+  <Fragment>
+    <Appointment id={1} time="4pm" />
+    <Appointment time="5pm" />
+  </Fragment>
 ))
