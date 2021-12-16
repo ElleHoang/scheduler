55bc7d95d8fbb2487ca2edbf30f8795bce8d580e
Linh Hoang <ellehoang@hotmail.com>
2021-12-15 22:51:26 -0500

    fetch interviewer data, transform interview data, and implement and test getInterview function

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,7 +6,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment";
 import Appointment from "./Appointment/index"
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 /*
 //mock days data
 const days = [
@@ -83,7 +83,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   // setDay function that updates state w/ new day
@@ -94,12 +95,20 @@ export default function Application(props) {
   const setDays = (days) => setState(prev => ({ ...prev, days }));
   */
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day).map(appointment =>
-    <Appointment
-      key={appointment.id}
-      {...appointment}
-    />
-  );
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  
+  const schedule = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   useEffect(() => {
     Promise.all([
@@ -108,11 +117,12 @@ export default function Application(props) {
       axios.get("/api/interviewers"),
     ])
     .then((all) => {
-      //console.log(all);
+      /** test request by loggin values of state.interviewers **/
+      //console.log(all[2].data);
       //const [days, appointments, interviewers] = all;
-      setState((prev) => (
-        { ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}
-      ));
+        setState((prev) => (
+          { ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}
+        ));
     })
   }, []);
 
@@ -143,7 +153,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {dailyAppointments}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -12,4 +12,14 @@ export function getAppointmentsForDay(state, day) {
   // 2. iterate through appt array for given day, comparing where it's id matches id of states.appointments and return that value
   // return array of appts for that day
   return apptArrDay[0].appointments.map((id) => state.appointments[id]);
-} 
+}
+
+export function getInterview(state, interview) {
+  // function will return object that contains interview data if it's passed an object that contains an "interviewer".
+  if (interview && interview.interviewer) {
+    const interviewer = state.interviewers[interview.interviewer];
+    return {...interview, interviewer};
+  }
+  //Otherwise, function should return "null"
+  return null;
+}
