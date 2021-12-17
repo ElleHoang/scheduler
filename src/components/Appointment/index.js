4c6bae34829031bc6802cfceb0ef955d688ce5b7
Linh Hoang <ellehoang@hotmail.com>
2021-12-17 04:27:02 -0500

    use useVisualMode Hook to render EMPTY, SHOW, and FORM components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,20 +5,39 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
+
+// mode constants
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  // when props.interview contains value, then we want to pass useVisualMode the SHOW mode
+  // if props.interview is empty then should pass EMPTY
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ?
+      {mode === EMPTY &&
+        <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-        :
-        <Empty />
-      }
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={props.interviewers}
+          onSave={() => console.log("Clicked onSave")}
+          onCancel={() => back()}
+        />
+      )}
     </article>
   );
 }
